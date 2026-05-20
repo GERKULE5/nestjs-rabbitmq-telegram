1c80ff93537f94cdb96f5d42dbcffd65bf8dd8f8
obscure <konstantinkochengin44@gmail.com>
2026-05-20 09:20:41 +0700

    feat(config): migrate to config service

--- src/common/config/rabbitmq/rabbitmq.config.ts
+++ src/common/config/rabbitmq/rabbitmq.config.ts
@@ -1,9 +1,10 @@
+import { ConfigService } from '@nestjs/config';
 import { RmqOptions, Transport } from '@nestjs/microservices';
 
-export const getRabbitmqConfig = (): RmqOptions => ({
+export const getRabbitmqConfig = (service: ConfigService): RmqOptions => ({
   transport: Transport.RMQ,
   options: {
-    urls: [process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'],
+    urls: [service.get<string>('RABBITMQ_URL') ?? 'amqp://localhost:5672'],
     queue: 'notification_queue',
     queueOptions: {
       durable: true,
--- src/producer/producer.module.ts
+++ src/producer/producer.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ClientsModule } from '@nestjs/microservices';
 import { ProducerController } from './controller/producer.controller';
 import { ProducerService } from './service/producer.service';
 import { getRabbitmqConfig } from '../common/config/rabbitmq/rabbitmq.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'RABBITMQ_SERVICE',
-        ...getRabbitmqConfig(),
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: getRabbitmqConfig,
       },
     ]),
   ],
